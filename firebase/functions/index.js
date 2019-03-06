46cb73644c54e853148529a907a50f6ef48e3693
Francois Greeff <francois.greeff@bglgroup.co.uk>
2019-03-06 20:47:08 +0000

    Remove unused code, add logs for debug

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -33,7 +33,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
     )
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body))
     console.log('Dialogflow intent: ' + agent.intent)
-    console.log('Dialogflow music parameters: ' + agent.parameters['Singer'])
 
     let conv = agent.conv() // Get Actions on Google library conv instance
 
@@ -177,11 +176,14 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
         let option = agent.contexts.find(function(obj) {
           return obj.name === 'actions.intent.option'
         })
+
+        //I don't think the code ever reaches this if-statement below
         if (
           option &&
           option.hasOwnProperty('parameters') &&
           option.parameters.hasOwnProperty('OPTION')
         ) {
+          console.log('>>>> selected option value: ', option.parameters.OPTION) // is the selected option value
           conv.data.meetupCount = parseInt(
             option.parameters.OPTION.replace('meetup ', '')
           )
@@ -333,39 +335,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       agent.add(conv)
     }
 
-    // Uncomment and edit to make your own intent handler
-    // uncomment `intentMap.set('your intent name here', yourFunctionHandler);
-    // below to get this function to be run when a Dialogflow intent is matched
-    function yourFunctionHandler(agent) {
-      agent.add(
-        `This message is from Dialogflow's Cloud Functions for Firebase editor!`
-      )
-      agent.add(
-        new Card({
-          title: `Title: this is a card title`,
-          imageUrl:
-            'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-          text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-          buttonText: 'This is a button',
-          buttonUrl: 'https://assistant.google.com/'
-        })
-      )
-      agent.add(new Suggestion(`Quick Reply`))
-      agent.add(new Suggestion(`Suggestion`))
-      agent.setContext({
-        name: 'weather',
-        lifespan: 2,
-        parameters: { city: 'Rome' }
-      })
-    }
-    // Uncomment and edit to make your own Google Assistant intent handler
-    // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-    // below to get this function to be run when a Dialogflow intent is matched
-    function googleAssistantHandler(agent) {
-      let conv = agent.conv() // Get Actions on Google library conv instance
-      conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-      agent.add(conv) // Add Actions on Google library responses to your agent's response
-    }
     // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
     // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
 
@@ -380,8 +349,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
     intentMap.set('show meetup list', listMeetups)
     intentMap.set('show meetup list - select.number', selectByNumberMeetup)
 
-    intentMap.set('your intent name here', yourFunctionHandler)
-    intentMap.set('your intent name here', googleAssistantHandler)
     agent.handleRequest(intentMap)
   }
 )
